c72ac50732cc36339c6e73aaa6753429c2218e67
Das <Amayaranjan_Das@dell.com>
2017-10-28 20:23:49 +0530

    modified verfication operation

--- flymojo/img/views.py
+++ flymojo/img/views.py
@@ -2,10 +2,13 @@ from django.views.decorators.csrf import csrf_exempt, csrf_protect, requires_csr
 from django.http import HttpResponse
 from django.http import JsonResponse
 from models import Kyc
+import random
 @csrf_exempt
 # Create your views here.
 def get_pan_details(request):
-	kyc = Kyc.objects.get()
-	print kyc
-	response = {'responseCode':'responseCode'}
-	return JsonResponse(response)
+
+	kyc = Kyc.objects.filter(verified = False, check_count__lt = 4)
+	random_num = random.randint(0, kyc.count() - 1)
+	kyc = kyc[random_num]
+	image = '/media/%s'%kyc.image.url
+	return JsonResponse({'response_code':'success','name':kyc.name,'pan_no':kyc.pan_no,'dob':kyc.dob,'image':image,'id':kyc.id})
