4dac61c4e0862b0f302da55e00048005b73b880a
Vinayak K <Vinayak.K@wdc.com>
2017-10-28 18:39:38 +0530

    Model __str__ and optional fields

--- flymojo/img/models.py
+++ flymojo/img/models.py
@@ -2,9 +2,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 
-ACTIVE = ((0,'Inactive'), (2, 'Active'),)
+OPTIONAL = {'blank': True, 'null': True}
+
+
 class Base(models.Model):
-    
+
+    ACTIVE = ((0,'Inactive'), (2, 'Active'),)
     active = models.PositiveIntegerField(choices = ACTIVE, default=2)
     created_on = models.DateTimeField(auto_now_add = True)
     modified_on = models.DateTimeField(auto_now = True)
@@ -21,15 +24,18 @@ class Base(models.Model):
 class Kyc(Base):
     merchant = models.ForeignKey('Merchant')
 
-    name = models.CharField(max_length=100)
-    pan_no = models.CharField(max_length=100)
-    dob = models.DateField()
+    name = models.CharField(max_length=100, **OPTIONAL)
+    pan_no = models.CharField(max_length=100, **OPTIONAL)
+    dob = models.DateField(**OPTIONAL)
 
     image = models.ImageField()
     confidence_level = models.IntegerField()
 
     check_count = models.IntegerField(default=0)
     verified = models.BooleanField()
+
+    def __str__(self):
+        return str(self.merchant)
 
 
 class Moderator(models.Model):
@@ -39,9 +45,15 @@ class Moderator(models.Model):
         feedbacks = FeedBack.objects.filter(master__moderator=self)
         return sum(i.action.points for i in feedbacks)
 
+    def __str__(self):
+        return str(self.user)
+
 
 class Merchant(models.Model):
     user = models.ForeignKey('auth.User')
+
+    def __str__(self):
+        return str(self.user)
 
 
 class Action(models.Model):
@@ -50,16 +62,25 @@ class Action(models.Model):
     action = models.IntegerField(choices=ACTION_CHOICES)
     points = models.IntegerField()
 
+    def __str__(self):
+        return self.get_action_display()
+
 
 class MasterFeedBack(models.Model):
     moderator = models.ForeignKey('Moderator')
     kyc = models.ForeignKey('Kyc')
-    
+
+    def __str__(self):
+        return '%s-%s' % (str(self.merchant), str(self.kyc))
+
 
 class FeedBack(models.Model):
     FIELD_CHOICES = ((0, 'Name'), (1, 'Pan No'),
                       (2, 'dob'))
     master = models.ForeignKey('MasterFeedBack')
     action = models.ForeignKey('Action')
-    old_info = models.CharField(max_length=100)
+    old_info = models.CharField(max_length=100, **OPTIONAL)
     field = models.IntegerField(choices=FIELD_CHOICES)
+
+    def __str__(self):
+        return '%s-%s-%s' % (str(self.master), str(self.action), str(self.field))
